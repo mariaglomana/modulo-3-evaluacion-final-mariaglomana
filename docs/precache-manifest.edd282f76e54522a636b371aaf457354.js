234533f53eb36db7fad066ed0b10333c1b0622d3
María García de Lomana <mariagarciadelomana@gmail.com>
2020-01-14 19:28:15 +0100

    add docs for gitHub Pages

---
+++ docs/precache-manifest.edd282f76e54522a636b371aaf457354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499ec02e490d78c091fdadece93629c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a71fff44ffae46e008ef",
+    "url": "./static/css/main.a946367e.chunk.css"
+  },
+  {
+    "revision": "36f8ef813bb1376a48e1",
+    "url": "./static/js/2.8e3f8d49.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.8e3f8d49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a71fff44ffae46e008ef",
+    "url": "./static/js/main.84216f56.chunk.js"
+  },
+  {
+    "revision": "28bbc1b77944e55f5cc7",
+    "url": "./static/js/runtime-main.d753a5ee.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
